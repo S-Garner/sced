7b54cdff669df463fa1f49b290ad4ae1ee8c495f
Ethan Stucker <ethan.stucker@proton.me>
2025-10-15 11:06:23 -0500

    Made input detection a little more modular

--- src/input/Input.h
+++ src/input/Input.h
@@ -1,25 +1,56 @@
 #include <GLFW/glfw3.h>
-#include "../core/Window.h"
-
 #include <iostream>
+#include "../core/Window.h"
 using namespace std;
 
 class Input {
 public:
-	void checkKeyboardInput(GLFWwindow*);
+	void checkKeyboardInput(GLFWwindow* window);
+
+	static void key_callback(GLFWwindow* window, int key, int scancode,
+							 int action, int mods);
 };
 
 void Input::checkKeyboardInput(GLFWwindow* window) {
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
 	while (!glfwWindowShouldClose(window)) {
-		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-			cout << "The W Key was press";
-		}
-
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-			glfwSetWindowShouldClose(window, GLFW_TRUE);
-		}
-
 		glfwSwapBuffers(window);
+
 		glfwPollEvents();
+	}
+
+	glfwTerminate();
+}
+
+inline void Input::key_callback(GLFWwindow* window, int key, int scancode,
+								int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+
+	const char* keyName = glfwGetKeyName(key, scancode);
+
+	if (action == GLFW_PRESS) {
+		if (keyName != NULL) {
+			cout << "Key pressed: " << keyName << endl;
+		} else {
+			// Fallback for special keys that don't have a name (like F1-F12)
+			cout << "Special key pressed: " << key << endl;
+		}
+	} else if (action == GLFW_RELEASE) {
+		if (keyName != NULL) {
+			cout << "Key released: " << keyName << endl;
+		} else {
+			cout << "Special key released: " << key << endl;
+		}
+	} else if (action == GLFW_REPEAT) {	 // Handling the repeat action
+		if (keyName != NULL) {
+			cout << "Key repeated: " << keyName << endl;
+		} else {
+			cout << "Special key repeated: " << key << endl;
+		}
 	}
 }
